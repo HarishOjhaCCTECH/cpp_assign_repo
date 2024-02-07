3d16ff3459edb626d9eef98b6befc3d022142ca8
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-07 21:49:24 +0530

    Delete assign_3-stl_reader directory

--- assign_3-stl_reader/src/Obj2stl.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "../headers/Obj2stl.h"
-#include "../headers/Triangulation.h"
-using namespace std;
-Obj2stl::Obj2stl(){} // default constructor
-Obj2stl::~Obj2stl(){} // destructor{
-void Obj2stl::convert(string filePath, Triangulation & ctriguObj){
-
-}
