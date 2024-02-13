211c7a27692353698e5e01113974c301bd10c186
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 11:29:01 +0530

    Update StlReader.cpp

--- assign_6-stl2obj/src/StlReader.cpp
+++ assign_6-stl2obj/src/StlReader.cpp
@@ -5,19 +5,13 @@
 #include <sstream>
 #include <vector>
 #include <map>
-
-Shapes3D::StlReader::StlReader() {}
-Shapes3D::StlReader::~StlReader() {}
-
-void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &triangulationObj)
-{
+Shapes3D::StlReader::StlReader() {} // constructor
+Shapes3D::StlReader::~StlReader() {} // destructor
+void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &triangulationObj){ // read the stl file to store the data in structured way
     std::ifstream file(filePath);
-
-    if (!file.is_open())
-    {
+    if (!file.is_open()){
         std::cout << "Error while opening the file." << std::endl;
     }
-
     int count = 0;
     int index1;
     int index2;
@@ -26,60 +20,42 @@ void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &trian
     std::string line;
     std::map<Point3D, int> pointIndexMap;
     std::map<Point3D, int> normalIndexMap;
-
-    while (getline(file, line))
-    {
-        if (line.find("facet normal") != std::string::npos)
-        {
+    while (getline(file, line)){
+        if (line.find("facet normal") != std::string::npos){
             double x, y, z;
             std::istringstream facetNormalStream(line);
             std::string keywordFacet1, keywordFacet2;
             facetNormalStream >> keywordFacet1 >> keywordFacet2 >> x >> y >> z;
-
             Point3D facet(x, y, z);
-
             auto iterator = normalIndexMap.find(facet);
-            if (iterator == normalIndexMap.end())
-            {
+            if (iterator == normalIndexMap.end()){
                 normalIndexMap[facet] = triangulationObj.uniqueNormals().size();
                 triangulationObj.uniqueNormals().push_back(facet);
             }
             normalIndex = normalIndexMap[facet];
         }
-
-        if (line.find("vertex") != std::string::npos)
-        {
+        if (line.find("vertex") != std::string::npos){
             double x, y, z;
             std::istringstream vertexStream(line);
             std::string keywordVertex;
             vertexStream >> keywordVertex >> x >> y >> z;
-
             Point3D point(x, y, z);
-
             auto iterator = pointIndexMap.find(point);
-            if (iterator == pointIndexMap.end())
-            {
+            if (iterator == pointIndexMap.end()){
                 pointIndexMap[point] = triangulationObj.uniquePoints().size();
                 triangulationObj.uniquePoints().push_back(point);
             }
-
-            if (count == 0)
-            {
+            if (count == 0){
                 index1 = pointIndexMap[point];
                 count++;
-            }
-            else if (count == 1)
-            {
+            }else if (count == 1){
                 index2 = pointIndexMap[point];
                 count++;
-            }
-            else if (count == 2)
-            {
+            }else if (count == 2){
                 index3 = pointIndexMap[point];
                 count++;
             }
-            if (count == 3)
-            {
+            if (count == 3){
                 Triangle triangle(index1, index2, index3);
                 triangle.setNormalIndex(normalIndex);
                 triangulationObj.triangles().push_back(triangle);
