3c997cd79f36573510ef5700ad6d7d887d698dd7
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 10:54:08 +0530

    Update StlReader.cpp

--- assign_7-mesh_quality_check/src/StlReader.cpp
+++ assign_7-mesh_quality_check/src/StlReader.cpp
@@ -5,20 +5,13 @@
 #include <sstream>
 #include <vector>
 #include <map>
-
-Shapes3D::StlReader::StlReader() {}
-Shapes3D::StlReader::~StlReader() {}
-
-void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &triangulation)
-{
+Shapes3D::StlReader::StlReader() {} // default constructor
+Shapes3D::StlReader::~StlReader() {} // destructor
+void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &triangulation) // read the stl file and store in data structure{
     std::ifstream file(filePath);
-
-    // checks for file opening error
-    if (!file.is_open())
-    {
+    if (!file.is_open()){
         std::cout << "Error while opening the file." << std::endl;
     }
-
     int count = 0;
     int index1;
     int index2;
@@ -26,53 +19,33 @@ void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &trian
     std::string line;
     std::string keyword;
     std::map<Point3D, int> pointIndexMap;
-
-    // reads obj file line by line
-    while (getline(file, line))
-    {
+    while (getline(file, line)){
         std::istringstream iss(line);
-
-        if (line.find("vertex") != std::string::npos)
-        {
+        if (line.find("vertex") != std::string::npos){
             double x;
             double y;
             double z;
             std::istringstream vertexStream(line);
             std::string keywordVertex;
             vertexStream >> keywordVertex >> x >> y >> z;
-
             Point3D point(x, y, z);
-
-            // searchs for point in the map
             auto iterator = pointIndexMap.find(point);
-            if (iterator == pointIndexMap.end())
-            {
-                // if not found in map, adds point to map
+            if (iterator == pointIndexMap.end()){
                 pointIndexMap[point] = triangulation.uniquePoints().size();
-                // adds point to triangulation object
                 triangulation.addUniquePointToTriangulation(point);
             }
-
-            // get vertex indices
-            if (count == 0)
-            {
+            if (count == 0){
                 index1 = pointIndexMap[point];
                 count++;
-            }
-            else if (count == 1)
-            {
+            }else if (count == 1){
                 index2 = pointIndexMap[point];
                 count++;
-            }
-            else if (count == 2)
-            {
+            }else if (count == 2){
                 index3 = pointIndexMap[point];
                 count++;
             }
-            if (count == 3)
-            {
+            if (count == 3){
                 Triangle triangle(index1, index2, index3);
-                // add triangle to triangulation object
                 triangulation.addTriangleToTriangulation(triangle);
                 count = 0;
             }
@@ -80,6 +53,5 @@ void Shapes3D::StlReader::read(const std::string &filePath, Triangulation &trian
     };
     std::cout << std::endl
               << "Data reading from .stl file completed successfully" << std::endl;
-    // closing the file
     file.close();
 }
