794aebdfc6ac4fe5476dcd22a6872a419d8971d4
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 11:10:35 +0530

    Update Triangle.h

--- assign_7-mesh_quality_check/headers/Triangle.h
+++ assign_7-mesh_quality_check/headers/Triangle.h
@@ -1,27 +1,22 @@
+// purpose - stores the points index which are in vector
 #pragma once
 #include "Point3D.h"
 #include <vector>
-
-namespace Shapes3D
-{
-    class Triangle
-    {
-    public:
-        Triangle();
-        Triangle(const int inIndex1, const int inIndex2, const int inIndex3);
-        ~Triangle();
-
-        int index1() const;
-        int index2() const;
-        int index3() const;
-
-        void setindex1(int inIndex1);
-        void setindex2(int inIndex2);
-        void setindex3(int inIndex3);
-
-    private:
-        int mIndex1;
-        int mIndex2;
-        int mIndex3;
+namespace Shapes3D{
+    class Triangle{
+        public:
+            Triangle(); // constructor
+            Triangle(const int inIndex1, const int inIndex2, const int inIndex3); // parameterised constructor
+            ~Triangle(); // destructor
+            int index1() const; // getter
+            int index2() const; // getter
+            int index3() const; // getter
+            void setindex1(int inIndex1); // setter
+            void setindex2(int inIndex2); // setter
+            void setindex3(int inIndex3); // setter
+        private:
+            int mIndex1;
+            int mIndex2;
+            int mIndex3;
     };
 }
