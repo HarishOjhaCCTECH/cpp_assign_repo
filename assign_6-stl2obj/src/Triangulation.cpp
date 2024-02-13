c123d08ff8161a8ebe9e1fb8bb59c97f7ea8a0c7
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 11:23:11 +0530

    Update Triangulation.cpp

--- assign_6-stl2obj/src/Triangulation.cpp
+++ assign_6-stl2obj/src/Triangulation.cpp
@@ -2,26 +2,21 @@
 #include "../headers/Point3D.h"
 #include "../headers/Triangle.h"
 #include <vector>
-
-Shapes3D::Triangulation::Triangulation()
-{
-}
-
-Shapes3D::Triangulation::~Triangulation()
-{
-}
-
-std::vector<Shapes3D::Point3D> &Shapes3D::Triangulation::uniquePoints()
-{
+Shapes3D::Triangulation::Triangulation() {} // default consructor
+Shapes3D::Triangulation::~Triangulation() {} // destructor
+// vector for storing unique points
+std::vector<Shapes3D::Point3D> Shapes3D::Triangulation::uniquePoints() const{
     return mUniquePoints;
 }
-
-std::vector<Shapes3D::Triangle> &Shapes3D::Triangulation::triangles()
-{
+// vector for storing triangles
+std::vector<Shapes3D::Triangle> Shapes3D::Triangulation::triangles() const{
     return mTriangles;
 }
-
-std::vector<Shapes3D::Point3D> &Shapes3D::Triangulation::uniqueNormals()
-{
-    return mUniqueNormals;
+// adds a point to mUniquePoints vector
+void Shapes3D::Triangulation::addUniquePointToTriangulation(const Shapes3D::Point3D inPoint){
+    mUniquePoints.push_back(inPoint);
+}
+// adds a triangle to mTriangles vector
+void Shapes3D::Triangulation::addTriangleToTriangulation(const Shapes3D::Triangle inTriangle){
+    mTriangles.push_back(inTriangle);
 }
