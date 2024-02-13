7299648f60140c5c06fe96a51dfbfdd8c4be232d
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 11:08:45 +0530

    Update Triangulation.h

--- assign_7-mesh_quality_check/headers/Triangulation.h
+++ assign_7-mesh_quality_check/headers/Triangulation.h
@@ -1,23 +1,19 @@
+// purpose - stores all the points and triangles in the mesh
 #pragma once
 #include "Point3D.h"
 #include "Triangle.h"
 #include <vector>
-
-namespace Shapes3D
-{
-    class Triangulation
-    {
-    public:
-        Triangulation();
-        ~Triangulation();
-
-        std::vector<Point3D> uniquePoints() const;
-        std::vector<Triangle> triangles() const;
-        void addUniquePointToTriangulation(const Point3D inPoint);
-        void addTriangleToTriangulation(const Triangle inTriangle);
-
-    private:
-        std::vector<Point3D> mUniquePoints;
-        std::vector<Triangle> mTriangles;
+namespace Shapes3D{
+    class Triangulation{
+        public:
+            Triangulation(); // constructor
+            ~Triangulation(); // destructor
+            std::vector<Point3D> uniquePoints() const; // getter
+            std::vector<Triangle> triangles() const; // getter
+            void addUniquePointToTriangulation(const Point3D inPoint);
+            void addTriangleToTriangulation(const Triangle inTriangle);
+        private:
+            std::vector<Point3D> mUniquePoints;
+            std::vector<Triangle> mTriangles;
     };
 }
