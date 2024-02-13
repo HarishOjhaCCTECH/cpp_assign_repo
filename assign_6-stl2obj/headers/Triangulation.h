eb2d0cfc673a5ce10f046732edfebf49158b2b6a
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 11:38:27 +0530

    Update Triangulation.h

--- assign_6-stl2obj/headers/Triangulation.h
+++ assign_6-stl2obj/headers/Triangulation.h
@@ -2,22 +2,17 @@
 #include "Point3D.h"
 #include "Triangle.h"
 #include <vector>
-
-namespace Shapes3D
-{
-    class Triangulation
-    {
-    public:
-        Triangulation();
-        ~Triangulation();
-
-        std::vector<Point3D> &uniquePoints();
-        std::vector<Triangle> &triangles();
-        std::vector<Point3D> &uniqueNormals();
-
-    private:
-        std::vector<Point3D> mUniquePoints;
-        std::vector<Triangle> mTriangles;
-        std::vector<Point3D> mUniqueNormals;
+namespace Shapes3D{
+    class Triangulation{
+        public:
+            Triangulation(); // constrcutor
+            ~Triangulation(); // destructor
+            std::vector<Point3D> &uniquePoints(); // getter
+            std::vector<Triangle> &triangles(); // getter
+            std::vector<Point3D> &uniqueNormals(); // getter
+        private:
+            std::vector<Point3D> mUniquePoints;
+            std::vector<Triangle> mTriangles;
+            std::vector<Point3D> mUniqueNormals;
     };
 }
