10de94fefd1e4abc7c5b361e1ea6548b659c1293
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 10:42:18 +0530

    Update main.cpp

--- assign_7-mesh_quality_check/main.cpp
+++ assign_7-mesh_quality_check/main.cpp
@@ -8,14 +8,10 @@
 
 int main()
 {
-    // std::string inputFilePath = "resources/cube.stl";
-    // std::string outputFilePath = "output/cube.txt";
-
     std::string inputFilePath = "resources/cube_withErrors.stl";
     std::string outputFilePath = "output/cube_forTesting.txt";
     std::string outputBadTrianglesFilePath = "output/cube_badTriangles.txt";
     std::string outputGoodTrianglesFilePath = "output/cube_goodTriangles.txt";
-
     Shapes3D::FileTypeChecker checker;
     if (!checker.isStlFile(inputFilePath))
     {
@@ -23,7 +19,6 @@ int main()
         std::cout << "---------------------------------------------------" << std::endl;
         return 1;
     }
-
     Shapes3D::Triangulation triangulation;
     Shapes3D::Triangulation triangulationBadTriangles;
     Shapes3D::Triangulation triangulationGoodTriangles;
@@ -31,25 +26,14 @@ int main()
     Shapes3D::StlReader stlReader;
     Shapes3D::TxtWriter txtWriter;
     Shapes3D::MeshQualityChecker meshQualityChecker(triangulation);
-
     stlReader.read(inputFilePath, triangulation);
     meshQualityChecker.calculateMeshDensity(triangulation);
     meshQualityChecker.calculateAspectRatio(triangulation);
     meshQualityChecker.calculateAspectRatioSkewness(triangulation);
     txtWriter.write(outputFilePath, triangulation);
-
     meshQualityChecker.categorizeTriangles(triangulation, triangulationBadTriangles, triangulationGoodTriangles);
     txtWriter.writeBadTriangles(outputBadTrianglesFilePath, triangulationBadTriangles);
     txtWriter.writeGoodTriangles(outputGoodTrianglesFilePath, triangulationGoodTriangles);
-
-    // meshQualityChecker.calculateAspectRatioDistribution(triangulation, 250);
     std::cout << "***************************************************" << std::endl;
     return 0;
 }
-
-/*
-read stl
-check
-depending on criteria - gve diff trian obj
-writer take tri obj from checker
-*/
