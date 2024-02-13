fadb53685f0afc7fbead98e968f6cfb32649d548
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-02-13 11:42:24 +0530

    Update StlReader.h

--- assign_6-stl2obj/headers/StlReader.h
+++ assign_6-stl2obj/headers/StlReader.h
@@ -1,16 +1,13 @@
+// purpose - read the stl file and store it in data structure
 #pragma once
 #include "Triangulation.h"
 #include <string>
 #include <vector>
-
-namespace Shapes3D
-{
-    class StlReader
-    {
-    public:
-        StlReader();
-        ~StlReader();
-
-        void read(const std::string &filePath, Triangulation &triangulationObj);
+namespace Shapes3D{
+    class StlReader{
+        public:
+            StlReader(); // constructor
+            ~StlReader(); // destructor
+            void read(const std::string &filePath, Triangulation &triangulationObj); // read the stl and stores in data structure
     };
 }
