91ce15609b3b1422f42a51bc70d0760a812d756c
HarishOjhaCCTECH <158055492+HarishOjhaCCTECH@users.noreply.github.com>
2024-03-30 18:58:20 +0530

    Add files via upload

---
+++ assign_5-obj2stl/src/ObjReader.cpp
@@ -0,0 +1,87 @@
+#include "../headers/ObjReader.h"
+#include "../headers/Triangulation.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+Shapes3D::ObjReader::ObjReader() {}
+Shapes3D::ObjReader::~ObjReader() {}
+
+void Shapes3D::ObjReader::read(const std::string &filePath, Triangulation &triangulationObj)
+{
+    std::ifstream file(filePath);
+
+    if (!file.is_open())
+    {
+        std::cout << "Error while opening .obj file." << std::endl;
+        return;
+    }
+
+    std::string line;
+    int normalIndex = 0;
+
+    // reads obj file line by line
+    while (getline(file, line))
+    {
+        std::istringstream vertexStream(line);
+        std::string keyword;
+        vertexStream >> keyword;
+
+        if (keyword == "v")
+        {
+            double x, y, z;
+            vertexStream >> x >> y >> z;
+            Point3D point(x, y, z);
+            triangulationObj.uniquePoints().push_back(point);
+        }
+        else if (keyword == "vn")
+        {
+            double x, y, z;
+            vertexStream >> x >> y >> z;
+            Point3D normal(x, y, z);
+            triangulationObj.uniqueNormals().push_back(normal);
+        }
+        else if (keyword == "f")
+        {
+            std::string index1, index2, index3;
+            vertexStream >> index1 >> index2 >> index3;
+
+            std::vector<std::string> tokens1, tokens2, tokens3;
+
+            std::stringstream vertexTokenStreamV(index1), vertexTokenStreamT(index2), vertexTokenStreamN(index3);
+            std::string token;
+            while (getline(vertexTokenStreamV, token, '/'))
+            {
+                tokens1.push_back(token);
+            }
+            while (getline(vertexTokenStreamT, token, '/'))
+            {
+                tokens2.push_back(token);
+            }
+            while (getline(vertexTokenStreamN, token, '/'))
+            {
+                tokens3.push_back(token);
+            };
+
+            int vertex1 = stoi(tokens1[0]) - 1;
+            int vertex2 = stoi(tokens2[0]) - 1;
+            int vertex3 = stoi(tokens3[0]) - 1;
+
+            for (int i = index1.size(); i > 0; i--)
+            {
+                if (index1[i] == '/')
+                {
+                    normalIndex = std::stoi(index1.substr(i + 1, index1.size() - i - 1));
+                    break;
+                }
+            }
+            std::cout << vertex1 << vertex2 << vertex3 << std::endl;
+            Triangle triangle(vertex1, vertex2, vertex3);
+            triangle.setNormalIndex(normalIndex - 1);
+            triangulationObj.triangles().push_back(triangle);
+        }
+    }
+    std::cout << "Data reading from .obj file completed successfully" << std::endl;
+    file.close();
+}
